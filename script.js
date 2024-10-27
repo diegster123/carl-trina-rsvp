cead3dafec2e1d940055fa2dc91f2d4eda6803da
diegster123 <diegojumagdao@gmail.com>
2024-10-27 08:35:29 +0800

    Add map

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function createMapModal() {
         <div class="map-modal-overlay"></div>
         <div class="map-modal-content">
           <button class="map-modal-close">&times;</button>
-         
+          <iframe src="https://www.google.com/maps/embed?pb=!1m34!1m12!1m3!1d6596.148732701084!2d120.98521710580653!3d14.610949428016117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m19!3e0!4m5!1s0x3397c9fffc6ffc0b%3A0x631402c0b2ea5f77!2sSant%C3%ADsimo%20Rosario%20Parish%20(Archdiocese%20of%20Manila)%2C%20JX5Q%2BM8G%2C%20Ceferino%20Gonzales%20Dr%2C%20Sampaloc%2C%20Manila%2C%201008%20Metro%20Manila!3m2!1d14.6091503!2d120.9883661!4m5!1s0x3397ca01d3cbb2c9%3A0xa7c7489ec1ddd6e8!2sPadre%20Campa%20St%2C%20Sampaloc%2C%20Manila%2C%201008%20Metro%20Manila!3m2!1d14.6064642!2d120.9876377!4m5!1s0x3397b5fd636fd041%3A0x3684f05d82fff018!2sWinford%20Resort%20and%20Casino%20Manila%2C%20352%20Zone%2035%2C%201800%20Consuelo%20St%2C%20Santa%20Cruz%2C%20Manila%2C%201014%20Metro%20Manila!3m2!1d14.6175328!2d120.9873913!5e0!3m2!1sen!2sph!4v1729988696215!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     `;
